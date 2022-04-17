a1934f5f3bbf93c665673fc84d9d0db79bc637a9
Mitchell M <38023623+mmichalak-swe@users.noreply.github.com>
2022-04-17 17:59:40 -0400

    refactor with dict.get method

---
+++ Generate_Document/attempt_2.py
@@ -0,0 +1,19 @@
+def generateDocument(characters, document):
+    letters = {}
+    
+    for char in characters:
+        # if char not in letters:
+        #     letters[char] = 1
+        # else:
+        #     letters[char] += 1
+        letters[char] = letters.get(char, 0) + 1
+
+    for char in document:
+        if char not in letters:
+            return False
+        elif letters[char] == 0:
+            return False
+        else:
+            letters[char] -= 1
+
+    return True
