d5285ac130af13fb4c7672d590b333920df2f543
Whitehotburn <38023623+Whitehotburn@users.noreply.github.com>
2022-04-21 17:02:44 -0400

    add modulo logic

--- Valid_Starting_City/attempt_1.py
+++ Valid_Starting_City/attempt_1.py
@@ -1,24 +1,21 @@
 # O(n^2) time | O(1) space
 
 def validStartingCity(distances, fuel, mpg):
+    num_of_cities = len(distances)
 
-    for i in range(len(distances)):
+    for i in range(num_of_cities):
         if (fuel[i] * mpg < distances[i]):
             continue
         else:
             current_range = 0
             curr_city = i
-            for j in range(len(distances)):
+            for j in range(i, i + num_of_cities):
                 current_range += mpg * fuel[curr_city]
                 current_range -= distances[curr_city]
 
                 if current_range < 0:
                     break
-
-                if j == len(distances) - 1:
+                if j == num_of_cities - 1:
                     return i
 
-                if curr_city < len(distances) - 1:
-                    curr_city += 1
-                else:
-                    curr_city = 0
+                curr_city = (curr_city + 1) % num_of_cities
