0135bc3cde59c4929ec23d7803351ad83fa3af37
Mitchell M <38023623+mmichalak-swe@users.noreply.github.com>
2022-04-30 00:28:42 -0400

    remove redundant operations from DFS function

---
+++ Depth_First_Search/attempt_2.py
@@ -0,0 +1,17 @@
+# O(V + E) Time | O(V) space
+# where V is # of vertices, E is # of edges
+
+class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+
+    def addChild(self, name):
+        self.children.append(Node(name))
+        return self
+
+    def depthFirstSearch(self, array):
+        array.append(self.name)
+        for child in self.children:
+            child.depthFirstSearch(array)
+        return array
