5695f13f622ecfe621d2c829809effcf609fd5fa
Whitehotburn <38023623+Whitehotburn@users.noreply.github.com>
2022-05-14 16:41:45 -0400

    refactoring

--- Product_Sum/attempt_1.py
+++ Product_Sum/attempt_1.py
@@ -1,13 +1,15 @@
 def productSum(array):
-    return productSumhelper(array, sum=0, depth=1)
+    return productSumhelper(array, depth=1)
 
 
-def productSumhelper(array, depth, sum=0):
+def productSumhelper(array, depth):
+
+    sum = 0
 
     for item in array:
         if type(item) is int:
-            sum += depth*item
+            sum += item
         else:
-            sum += productSumhelper(item, (depth*(depth+1)))
+            sum += productSumhelper(item, depth+1)
 
-    return sum
+    return sum * depth
