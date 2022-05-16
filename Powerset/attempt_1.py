f555d7815d694d724d852e09d28609758568d91a
Whitehotburn <38023623+Whitehotburn@users.noreply.github.com>
2022-05-16 17:54:06 -0400

    Powerset iterative attempt

---
+++ Powerset/attempt_1.py
@@ -0,0 +1,11 @@
+def powerset(array):
+    ans = [[]]
+    
+    for i in range(len(array)):
+        curr = ans[:]
+        for item in curr:
+            temp = item[:]
+            temp.append(array[i])
+            ans.append(temp)
+    
+    return ans
