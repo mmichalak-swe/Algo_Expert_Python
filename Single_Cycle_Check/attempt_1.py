7d79c37ffde340f5f47ccc0e1aa3d8903733ab8b
Mitchell <38023623+mmichalak-swe@users.noreply.github.com>
2022-06-03 19:59:25 -0400

    optimize to not overwrite array, and modulo logic

--- Single_Cycle_Check/attempt_1.py
+++ Single_Cycle_Check/attempt_1.py
@@ -6,15 +6,13 @@ def hasSingleCycle(array):
         # handle loop between idx = 0 and idx = something else
         if (0 < i < length - 1) and idx == 0:
             return False
-        
+
         # array[idx] %= length       # modifies the existing array
-        idx += (array[idx] % length) # does not modify existing array
+        idx = (idx + array[idx]) % length # does not modify existing array
 
-        idx += array[idx]
-
-        if idx > (length - 1):
-            idx -= length
-        elif idx < 0:
+        # if idx > (length - 1):
+            # idx -= length
+        if idx < 0:
             idx += length
 
     return (idx == 0)
