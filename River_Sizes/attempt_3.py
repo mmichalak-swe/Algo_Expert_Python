903a8dcfcf8e8043206ccba1f8fe0ce49ba81cae
Mitchell <38023623+mmichalak-swe@users.noreply.github.com>
2022-06-05 21:59:35 -0400

    updated for loop syntax for clarity

--- River_Sizes/attempt_3.py
+++ River_Sizes/attempt_3.py
@@ -1,9 +1,6 @@
 #             SOUTH   EAST    NORTH    WEST
 DIRECTIONS = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
-# O(wh) time | O(wh) space - where w is width of matrix,
-# h is height of matrix
-# OR O(n) time | O(n) space where n is total # of nodes
 def riverSizes(matrix):
 
     riverSizes = []
@@ -38,8 +35,8 @@ def riverSizes(matrix):
 
 
 def getValidSearch(nodesToSearch, matrix, row, col, alreadyVisited):
-    for i in range(4): # number/length of DIRECTIONS
-        new_row , new_col = row + DIRECTIONS[i][0], col + DIRECTIONS[i][1]
+    for direction in DIRECTIONS: # number/length of DIRECTIONS
+        new_row , new_col = row + direction[0], col + direction[1]
         if not (0 <= new_row < len(matrix)) or not (0 <= new_col < len(matrix[0])):
             continue
         else:
