2157b6badec6c96b534707a666b188c82ecc4a11
Mitchell <38023623+mmichalak-swe@users.noreply.github.com>
2022-06-11 23:44:44 -0400

    more efficient solutions

---
+++ Multi_String_Search/attempt_3.py
@@ -0,0 +1,40 @@
+def multiStringSearch(bigString, smallStrings):
+    output = [False for _ in smallStrings]
+    trie = makeTrieString(smallStrings)
+    # print(trie)
+    idx = 0
+    while idx < len(bigString):
+        char = bigString[idx]
+        if char == " " or char not in trie:
+            idx += 1
+            continue
+
+        node = trie
+        j = idx
+        possibility = []
+        while j < len(bigString):
+            if bigString[j] in node:
+                possibility.append(bigString[j])
+                node = node[bigString[j]]
+            else:
+                break
+            if ''.join(possibility) in smallStrings:
+                wordInSmall = ''.join(possibility)
+                output[smallStrings.index(wordInSmall)] = True
+            if node == {}:
+                break
+            j += 1
+        idx += 1
+
+    return output
+
+
+def makeTrieString(smallStrings):
+    trie = {}
+    for word in smallStrings:
+        node = trie
+        for char in word:
+            if char not in node:
+                node[char] = {}
+            node = node[char]
+    return trie
