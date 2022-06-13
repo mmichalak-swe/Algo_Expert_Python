8a624ac08b5e61179310abfe4d3692b1447aca99
Mitchell <38023623+mmichalak-swe@users.noreply.github.com>
2022-06-13 17:56:59 -0400

    added clearer return statement

---
+++ Num_Ways_To_Make_Change/attempt_3.py
@@ -0,0 +1,13 @@
+# O(nd) time | O(n) space, d is # of denoms,
+# n is len(waysChange) array, depends on argument n
+def numberOfWaysToMakeChange(n, denoms):
+    # need 0 as the base case (1 way to make 0, use 0 coins)
+    waysChange = [0 for _ in range(n+1)]
+    waysChange[0] = 1  # only one way to make an amount of zero
+
+    for denom in denoms:
+        for amount in range(0, n+1):
+            if denom <= amount:
+                waysChange[amount] += waysChange[amount - denom]
+
+    return waysChange[n]
