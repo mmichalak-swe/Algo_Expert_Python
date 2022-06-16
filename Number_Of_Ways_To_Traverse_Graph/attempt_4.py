3fd4d41ba236390c377ee7038b5ccacd43a7c152
Mitchell <38023623+mmichalak-swe@users.noreply.github.com>
2022-06-16 17:37:46 -0400

    bonus, advanced solution

---
+++ Number_Of_Ways_To_Traverse_Graph/attempt_4.py
@@ -0,0 +1,12 @@
+# BONUS Solution - Math Trick
+# O(wh) time | O(1) space
+from math import factorial
+
+def numberOfWaysToTraverseGraph(width, height):
+    right = width - 1
+    down = height - 1
+
+    numerator = factorial(right + down)
+    denominator = factorial(right) * factorial(down)
+
+    return numerator / denominator
