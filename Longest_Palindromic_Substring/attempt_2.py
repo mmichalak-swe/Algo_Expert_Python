0dabce73d91f4b8bad9010679aa4d908da4ebc6d
Mitchell <38023623+mmichalak-swe@users.noreply.github.com>
2022-06-29 16:55:32 -0400

    6/29/22 - more optimal solutions

--- Longest_Palindromic_Substring/attempt_2.py
+++ Longest_Palindromic_Substring/attempt_2.py
@@ -1,7 +1,9 @@
-# O(n) time | O(n) space
+# O(n^2) time | O(n) space, where n is length of input string
+# worst case O(n) space, if entire input string is longest palindrome
+# will take more time than optimal due to lines 35 & 36
 def longestPalindromicSubstring(string):
-    long = ""
-    for i in range(len(string)):
+    long = ""                           # O(n) space
+    for i in range(len(string)):        # O(n^2) time
         temp = isPalindrome(i, string)
         if len(temp) > len(long):
             long = temp
@@ -10,32 +12,29 @@ def longestPalindromicSubstring(string):
 
 
 def isPalindrome(idx, input):
-    oddOffset = 0
-
-    # odd length Palindrome logic
-    while idx - oddOffset > 0 and idx + oddOffset < len(input):
-        if input[idx - oddOffset] == input[idx + oddOffset]:
-            oddOffset += 1
-        else:
-            break
-    if 0 < idx < len(input):
-        oddOffset -= 1
-    
+    oddOffset = 1
     l_offset = 1
     r_offset = 0
+    if 1 <= idx < len(input):
+        while True:
+            if idx - oddOffset >= 0 and idx + oddOffset < len(input) and input[idx - oddOffset] == input[idx + oddOffset]:
+                oddOffset += 1
+            else:
+                oddOffset -= 1
+                break
 
-    # even length Palindrome logic
-    while idx - l_offset > 0 and idx + r_offset < len(input):
-        if input[idx-l_offset] == input[idx+r_offset]:
-            l_offset += 1
-            r_offset += 1
-        else:
-            break
-    l_offset -= 1
-    r_offset -= 1
+        while True:
+            if idx - l_offset >= 0 and idx + r_offset < len(input) and input[idx - l_offset] == input[idx + r_offset]:
+                l_offset += 1
+                r_offset += 1
+            else:
+                l_offset -= 1
+                r_offset -= 1
+                break
 
-    oddResult = input[idx-oddOffset : idx+oddOffset+1]
-    evenResult = input[idx-l_offset : idx+r_offset+1]
-    return max(oddResult, evenResult, key=len)
+        oddPal = input[idx-oddOffset : idx+oddOffset+1]
+        evenPal = input[idx-l_offset : idx+r_offset+1]
 
-print(longestPalindromicSubstring("aca"))
+        return max(oddPal, evenPal, key=len)
+    else:
+        return input[idx]
