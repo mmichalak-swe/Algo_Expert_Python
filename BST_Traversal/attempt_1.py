80c97b222b06a3759fdbfc442c828ee40aaecdac
Mitchell <38023623+mmichalak-swe@users.noreply.github.com>
2022-07-12 18:32:58 -0400

    7/12/22

---
+++ BST_Traversal/attempt_1.py
@@ -0,0 +1,29 @@
+# SAME BIG O FOR ALL THREE
+# O(n) time | O(n) space, where n is the # of nodes in the BST
+def inOrderTraverse(tree, array):
+    if tree is None:
+        return
+
+    inOrderTraverse(tree.left, array)
+    array.append(tree.value)
+    inOrderTraverse(tree.right, array)
+    return array
+
+
+def preOrderTraverse(tree, array):
+    if tree is None:
+        return
+
+    array.append(tree.value)
+    preOrderTraverse(tree.left, array)
+    preOrderTraverse(tree.right, array)
+    return array
+
+def postOrderTraverse(tree, array):
+    if tree is None:
+        return
+
+    postOrderTraverse(tree.left, array)
+    postOrderTraverse(tree.right, array)
+    array.append(tree.value)
+    return array
---
+++ BST_Traversal/attempt_2.py
@@ -0,0 +1,23 @@
+# SAME BIG O FOR ALL THREE
+# O(n) time | O(n) space, where n is the # of nodes in the BST
+def inOrderTraverse(tree, array):
+    if tree is not None:
+        inOrderTraverse(tree.left, array)
+        array.append(tree.value)
+        inOrderTraverse(tree.right, array)
+    return array
+
+
+def preOrderTraverse(tree, array):
+    if tree is not None:
+        array.append(tree.value)
+        preOrderTraverse(tree.left, array)
+        preOrderTraverse(tree.right, array)
+    return array
+
+def postOrderTraverse(tree, array):
+    if tree is not None:
+        postOrderTraverse(tree.left, array)
+        postOrderTraverse(tree.right, array)
+        array.append(tree.value)
+    return array
