5a0d7adf025754ce9a6dd0d74e100c351476b8fb
Mitchell <38023623+mmichalak-swe@users.noreply.github.com>
2022-07-15 20:03:11 -0400

    7/15/22

---
+++ Reconstruct_BST/attempt_1.py
@@ -0,0 +1,32 @@
+# This is an input class. Do not edit.
+class BST:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+class Index:
+    def __init__(self, index):
+        self.index = index
+
+# O(n) time | O(n) space, where n is the length of the input array
+def reconstructBst(preOrderTraversalValues):
+    tracker = Index(0)
+    return reconstructBstHelper(preOrderTraversalValues, float('-inf'), float('inf'), tracker)
+
+
+def reconstructBstHelper(values, lowerBound, upperBound, tracker, currNode=None):
+    if tracker.index >= len(values):
+        return
+
+    if lowerBound <= values[tracker.index] < upperBound:
+        currNode = BST(values[tracker.index])
+        tracker.index += 1
+    else:
+        return
+
+    currNode.left = reconstructBstHelper(values, lowerBound, currNode.value, tracker, currNode.left)
+    currNode.right = reconstructBstHelper(values, currNode.value, upperBound, tracker, currNode.right)
+    
+    return currNode
