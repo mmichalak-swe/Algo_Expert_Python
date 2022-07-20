f5488b09f1b81fe9441af44ef52827092f137770
Mitchell <38023623+mmichalak-swe@users.noreply.github.com>
2022-07-20 17:31:17 -0400

    7/20/22

---
+++ Quicksort/attempt_1.py
@@ -0,0 +1,38 @@
+# TIME:
+# Best: O(n*log(n)), Average: O(n*log(n)), Worst: O(n^2)
+# SPACE:
+# Best/Average: O(log(n)), Worst: O(n)
+def quickSort(array):
+    quickSortHelper(array, 0, len(array) - 1)
+    return array
+
+
+def quickSortHelper(array, startIdx, endIdx):
+    if startIdx >= endIdx:
+        return
+
+    pivotIdx = startIdx
+    leftIdx = startIdx + 1
+    rightIdx = endIdx
+
+    while rightIdx >= leftIdx:
+        if array[leftIdx] > array[pivotIdx] and array[rightIdx] < array[pivotIdx]:
+            swap(array, leftIdx, rightIdx)
+        if array[leftIdx] <= array[pivotIdx]:
+            leftIdx += 1
+        if array[rightIdx] >= array[pivotIdx]:
+            rightIdx -= 1
+
+    swap(array, pivotIdx, rightIdx)
+    rightSideSmaller = rightIdx - 1 - startIdx > endIdx - (rightIdx) + 1
+
+    if rightSideSmaller:
+        quickSortHelper(array, rightIdx + 1, endIdx)
+        quickSortHelper(array, startIdx, rightIdx - 1)
+    else:
+        quickSortHelper(array, startIdx, rightIdx - 1)
+        quickSortHelper(array, rightIdx + 1, endIdx)
+
+
+def swap(array, i, j):
+    array[i], array[j] = array[j], array[i]
