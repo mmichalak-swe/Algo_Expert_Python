40acbe0a1a7eab4172848355978d6607dd8ba328
Mitchell <38023623+mmichalak-swe@users.noreply.github.com>
2022-07-24 18:36:07 -0400

    more concise

--- First_Duplicate_Value/attempt_2.py
+++ First_Duplicate_Value/attempt_2.py
@@ -4,9 +4,8 @@ def firstDuplicateValue(array):
     found = {}
 
     for num in array:
-        if num not in found:
-            found[num] = True
-        else:
+        if num in found:
             return num
+        found[num] = True
 
     return -1
