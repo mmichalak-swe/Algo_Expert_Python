989b462657c82b4157342b1f70e24c074b3c1a13
Mitchell <38023623+mmichalak-swe@users.noreply.github.com>
2022-07-28 19:23:03 -0400

    added solution with explicit use of a stack

---
+++ Sunset_Views/attempt_3.py
@@ -0,0 +1,18 @@
+# O(n) time | O(n) space, where n is the length of the input array
+# using a stack
+def sunsetViews(buildings, direction):
+
+    views = []
+    step = 1 if direction == 'EAST' else -1
+    idx = 0 if direction == 'EAST' else len(buildings) - 1
+
+    while idx >= 0 and idx < len(buildings):
+        currHeight = buildings[idx]
+
+        while len(views) > 0 and buildings[views[-1]] <= currHeight:
+            views.pop()
+
+        views.append(idx)
+        idx += step
+        
+    return views if direction == 'EAST' else list(reversed(views))
