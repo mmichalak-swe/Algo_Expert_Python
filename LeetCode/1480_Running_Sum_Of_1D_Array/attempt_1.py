228604784bffc92013bd31f13aa92822bc96ccfe
Mitchell M <38023623+mmichalak-swe@users.noreply.github.com>
2022-09-05 22:24:04 -0400

    9/5/22

---
+++ LeetCode/1480_Running_Sum_Of_1D_Array/attempt_1.py
@@ -0,0 +1,7 @@
+# O(n) time | O(1) space
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        for i in range(1, len(nums)):
+            nums[i] += nums[i-1]
+            
+        return nums
